46a3e882e64a2d2f2541a4e1b77b2ca99a6de5b7
«Ksenia <ksen29ph@gmail.com>
2023-04-23 21:50:45 +0300

    HW_4 Task_1 done

--- HW_4/Task_1.py
+++ HW_4/Task_1.py
@@ -5,3 +5,7 @@
 # Пользователь вводит 2 числа. n - кол-во элементов первого множества.
 # m - кол-во элементов второго множества. Затем пользователь вводит сами элементы множеств.
 
+n,m = input().split()
+first = [int(a) for a in input().split()]
+second = [int(b) for b in input().split()]
+print(*sorted(set(first).intersection(second)))
