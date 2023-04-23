4a239bc5f1541cee3c4735d61152a9e7e4d6c325
«Ksenia <ksen29ph@gmail.com>
2023-04-23 22:01:42 +0300

    HW_4 Task_2 done

--- HW_4/Task_2.py
+++ HW_4/Task_2.py
@@ -7,3 +7,13 @@
 # Напишите программу для нахождения максимального числа ягод, которое может собрать за один заход
 # собирающий модуль, находясь перед некоторым кустом заданной во входном файле грядки.
 
+n = int(input())
+arr = list()
+for i in range(n):
+    a = int(input())
+    arr.append(a)
+arr_count = list()
+for i in range(len(arr)-1):
+    arr_count.append(arr[i-1]+arr[i]+arr[i+1])
+arr_count.append(arr[-2]+arr[-1]+arr[0])
+print(max(arr_count))        
